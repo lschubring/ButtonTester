1b693be738322203af2b75ae342708424a9d4104
Leo Schubring <leo.schubring@outlook.de>
2019-09-03 20:24:10 +0100

    Added new Buutons(CloseButton, TextWindow, NewWindow)

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ButtonTester
@@ -32,6 +25,23 @@ namespace ButtonTester
         {
             MessageBox.Show("Dies ist der Test eines Buttons!");
 
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            var f2 = new Form1();
+            f2.Show();
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dies ist der Test eines Buttons!");
         }
     }
 }
